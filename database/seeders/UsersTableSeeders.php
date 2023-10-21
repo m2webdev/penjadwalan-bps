0a42362711f950e57a3a5d367ce3a7550f7de0f5
zYuna646 <anggarangga646@gmail.com>
2023-10-21 13:57:42 +0800

    add new view

--- database/seeders/UsersTableSeeders.php
+++ database/seeders/UsersTableSeeders.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeders extends Seeder
 {
@@ -12,6 +15,24 @@ class UsersTableSeeders extends Seeder
      */
     public function run(): void
     {
-        //
+        User::factory()->create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'jk' => 'laki-laki',
+            'role' => 'admin',
+            'password' => Hash::make('12345'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->create([
+            'name' => 'pengguna',
+            'username' => 'pengguna',
+            'jk' => 'laki-laki',
+            'role' => 'pengguna',
+            'password' => Hash::make('12345'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(["signin"])->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('home');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/auth/logout', [SessionController::class, 'logout'])->name('logout');
+
+    Route::get('/account', [UserController::class, 'index'])->name('akun.index');
+    Route::post('/account/create', [UserController::class, 'create'])->name('akun.create');
+    Route::delete('/account/delete/{id}', [UserController::class, 'delete'])->name('akun.delete');
+    Route::put('/account/{id}/update', [UserController::class, 'update'])->name('akun.update');
 });
 
 Route::get('/auth', [SessionController::class, 'index']);
